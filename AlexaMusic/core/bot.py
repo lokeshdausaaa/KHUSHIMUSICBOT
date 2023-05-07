49cc3c370f9d144fb4b8010dcb2bb4880e45340f
devidop <116952794+devidop@users.noreply.github.com>
2023-05-07 15:22:56 +0530

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -1,12 +1,10 @@
-#
-# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+
 
 # Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
-# All rights reserved. © Alexa © Yukki
+# devil server © @new_devil_world
+# Owner devil server
+# white devil
+# All rights reserved. © rose © Yukki
 
 
 import sys

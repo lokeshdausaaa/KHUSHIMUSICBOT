29e019b3bd641306a8717626de932187e919719c
Khushijha5544 <133341324+Khushijha5544@users.noreply.github.com>
2023-05-24 13:35:14 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -38,7 +38,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/devidop/rose",
+    "https://github.com/Khushijha5544/KHUSHIMUSICBOT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 

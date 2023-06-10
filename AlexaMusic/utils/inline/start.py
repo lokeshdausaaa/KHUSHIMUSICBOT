b310bd3f9edbadfe9470eb715dee8e5327ea6d24
Khushijha5544 <133341324+Khushijha5544@users.noreply.github.com>
2023-06-10 08:17:01 +0530

    Update start.py

--- AlexaMusic/utils/inline/start.py
+++ AlexaMusic/utils/inline/start.py
@@ -77,7 +77,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     if GITHUB_REPO and OWNER:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
+                InlineKeyboardButton(text=_["S_B_7"], user_id=5818840309),
                 InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
             ]
         )

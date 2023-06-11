69582082005befc5e0f51b466f196652b681c4b1
Khushijha5544 <133341324+Khushijha5544@users.noreply.github.com>
2023-06-11 10:06:06 +0530

    Update help.py

--- AlexaMusic/utils/inline/help.py
+++ AlexaMusic/utils/inline/help.py
@@ -17,13 +17,13 @@ from AlexaMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close")]
+    first = [InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
     second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
             callback_data="help_back",
         ),
-        InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close"),
+        InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
     ]
     mark = second if START else first
     upl = InlineKeyboardMarkup(
@@ -72,7 +72,7 @@ def help_back_markup(_):
                     text=_["BACK_BUTTON"],
                     callback_data=f"settings_back_helper",
                 ),
-                InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"),
+                InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"
             ]
         ]
     )

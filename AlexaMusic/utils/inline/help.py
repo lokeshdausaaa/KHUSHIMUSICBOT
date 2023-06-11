1fb9470729ff83d744caf4dd706d0b902acb2915
Khushijha5544 <133341324+Khushijha5544@users.noreply.github.com>
2023-06-11 10:25:18 +0530

    Add files via upload

--- AlexaMusic/utils/inline/help.py
+++ AlexaMusic/utils/inline/help.py
@@ -1,11 +1,6 @@
 #
 # Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
 # A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
 # All rights reserved. © Alisha © Alexa © Yukki
 
 
@@ -17,15 +12,13 @@ from AlexaMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
-   )
- ]
-second = [
+    first = [InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close")]
+    second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
             callback_data="help_back",
         ),
-        InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close"),
     ]
     mark = second if START else first
     upl = InlineKeyboardMarkup(
@@ -55,6 +48,12 @@ second = [
                     text=_["H_B_7"],
                     callback_data="help_callback hb7",
                 ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_8"],
+                    callback_data="help_callback hb8",
+                ),
                 InlineKeyboardButton(
                     text=_["H_B_6"],
                     callback_data="help_callback hb5",
@@ -74,7 +73,7 @@ def help_back_markup(_):
                     text=_["BACK_BUTTON"],
                     callback_data=f"settings_back_helper",
                 ),
-                InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"
+                InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"),
             ]
         ]
     )
